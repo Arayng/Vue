a22467102cb59cbe757e9b379373aaf2ce60b71e
Arayng <akddnjf12@gmail.com>
2023-01-16 10:58:11 +0900

    2023-01-16

--- gallery/src/App.vue
+++ gallery/src/App.vue
@@ -1,56 +1,60 @@
 <template>
-  <div id="app">
-    <v-app-bar app color="#adb5bd" fixed>
-      <v-app-bar-nav-icon ></v-app-bar-nav-icon>
-      <v-toolbar-title>K gallery</v-toolbar-title>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          transition="scale-transition"
+          width="40"
+        />
+
+        <v-img
+          alt="Vuetify Name"
+          class="shrink mt-1 hidden-sm-and-down"
+          contain
+          min-width="100"
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+          width="100"
+        />
+      </div>
+
       <v-spacer></v-spacer>
-      <v-btn icon>
-        <v-icon>mdi-dots-vertical</v-icon>
+
+      <v-btn
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+        text
+      >
+        <span class="mr-2">Latest Release</span>
+        <v-icon>mdi-open-in-new</v-icon>
       </v-btn>
     </v-app-bar>
-    <v-main>
-      <v-container>
-        <v-row>
-          <v-col cols="12" sm="6" md="4" lg="3" xl="2" v-for="i in this.kPicturs" :key="i.key">
-            <v-card height="330px">
-              <v-img :src="i.url" height="200px"></v-img>
-              <v-card-title>
-                <h1 class="title grey--text text--darken-3 mb-3">{{ i.title }}</h1>
-                <p class="body-2 grey--text">{{ i.info }}</p>
-              </v-card-title>
-            </v-card>
-          </v-col>
-        </v-row>
-      </v-container>
-    </v-main>
-    <v-footer>
 
-    </v-footer>
-  </div>
+    <v-main>
+      <HelloWorld/>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import DataSources from './datasources/picture-data'
+import HelloWorld from './components/HelloWorld';
 
 export default {
   name: 'App',
-  data(){
-    return {
-      kPicturs : DataSources.pictures
-    }
-  },
-  components: {
-  }
-}
-</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  components: {
+    HelloWorld,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
